5cc8f0ce7a6c16129b4fb56d74af78e0518a4190
axce-j <johnobi699@gmail.com>
2024-04-08 16:30:54 +0200

    changed my tailwind custom screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'mobile':{'min':'2px' },
+      'lgMobile':{'min':'376px' },
+      'sm': {'min': '640px' },
+      'md': {'min': '768px' },
+      'lg': {'min': '1024px' },
+      'xl': {'min': '1280px' },
+      'ml': {'min':'1762px'},
+
+    },
     extend: {},
   },
   plugins: [],
