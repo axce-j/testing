00485c26128f6f8c094275d2f7884f875d75f7b5
axce-j <johnobi699@gmail.com>
2024-04-08 11:42:08 +0200

    trying browser router

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
+ 
 // https://vitejs.dev/config/
 export default defineConfig({
   base:"/tutorial",
-  plugins: [react(),ghPages(),],
+  plugins: [react()],
 })
 // import { defineConfig } from 'vite'
 // import React from '@vitejs/plugin-react'
