cf310ecc448b114b9a859f792e636925c8a0dec4
axce-j <johnobi699@gmail.com>
2024-04-08 11:45:00 +0200

    using hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import {  BrowserRouter, HashRouter} from "react-router-dom";
+import {  HashRouter} from "react-router-dom";
  import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
- 
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <React.StrictMode>
+    <HashRouter basename="/tutorial">
     <ChakraProvider>
       <App />
     </ChakraProvider>
-    </BrowserRouter>
-    
+    </HashRouter>
+   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base:"/tutorial",
   plugins: [react()],
 })
-// import { defineConfig } from 'vite'
-// import React from '@vitejs/plugin-react'
-// import { ghPages } from 'vite-plugin-gh-pages'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/portfolio-react-threejs",
-//   plugins: [
-//     react(),
-//     ghPages(),
-//   ],
-// });
