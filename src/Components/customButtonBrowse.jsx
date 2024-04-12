4a06fc15a4df4044032442a51129dd7ca3a767f3
axce-j <johnobi699@gmail.com>
2024-04-12 17:02:14 +0200

    began the filter page,implemented pagination in the news page but havent set it to reset automatically,tried setting pagination for home but encoutered a bug,put thefiter tags in the browse page ,set its responsiveness api for browse page ready

--- src/Components/customButtonBrowse.jsx
+++ src/Components/customButtonBrowse.jsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 const CustomButtonBrowse = ({ classname,children,onClick,style}) => {
     const buttonClasses=`${classname}`
   return (
-  <button className={`${buttonClasses} hover:bg-[rgba(112,110,122,.6)] text-start pl-6 text-xs rounded-md w-40 lg:w-64 md:w-48 mobile:w-24 text-[10px] h-6 bg-gray-700`} onClick={onClick} style={style}>
+  <button className={`${buttonClasses} hover:bg-[rgba(112,110,122,.6)] text-start pl-6 text-xs rounded-md w-40 lg:w-64 md:w-64 lgMobile:w-48 mobile:w-24 text-[10px] h-6 bg-gray-700`} onClick={onClick} style={style}>
     {children}
     </button>
   )
