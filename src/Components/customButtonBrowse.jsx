84278757683750e607daf11a7f40947fe70a030b
axce-j <johnobi699@gmail.com>
2024-04-29 19:11:52 +0300

    fixed responsiveness of browspage filter options

--- src/Components/customButtonBrowse.jsx
+++ src/Components/customButtonBrowse.jsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 const CustomButtonBrowse = ({ classname,children,onClick,style}) => {
     const buttonClasses=`${classname}`
   return (
-  <button className={`${buttonClasses} hover:bg-[rgba(112,110,122,.6)] text-start pl-6 text-xs rounded-md w-40 lg:w-48 md:w-48  mobile:w-24 mobile:text-[8px] h-6 bg-gray-700`} onClick={onClick} style={style}>
+  <button className={`${buttonClasses} hover:bg-[rgba(112,110,122,.6)] text-start pl-6 text-xs rounded-md w-40 lg:w-48 md:w-[150px]  mobile:w-24 mobile:text-[8px] h-6 bg-gray-700`} onClick={onClick} style={style}>
     {children}
     </button>
   )
